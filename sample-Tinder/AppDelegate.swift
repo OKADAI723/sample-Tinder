0cec9fa43489e1d27b17b82db92eabf505170737
okadai <okadai5150723@icloud.com>
2021-07-29 13:46:00 +0900

    Initial Commit

---
+++ sample-Tinder/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  sample-Tinder
+//
+//  Created by Yudai Fujioka on 2021/07/29.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        window?.rootViewController = HomeViewController()
+        return true
+    }
+
+
+
+}
+
